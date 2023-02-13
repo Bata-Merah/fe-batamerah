1fa003c1a91807c268ba5ab4460f62ec628b3007
unknown <tuffahati393@gmail.com>
2023-02-13 21:36:48 +0700

    set up environtment

---
+++ src/View/LandingPage.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+function LandingPage() {
+//   const [count, setCount] = useState(0)
+
+  return (
+    <div className="LandingPage">
+        <h1 className="">hello</h1>
+        <p className='font-bold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis vel fuga assumenda eius tempore, nisi, nostrum pariatur sit id qui sapiente beatae reiciendis perferendis quisquam deserunt? Dolorem praesentium cum quae?</p>
+    </div>
+  )
+}
+
+export default LandingPage
